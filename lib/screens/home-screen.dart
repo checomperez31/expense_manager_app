72bbfcc9d9d6f8d3c36913966adf94ff24c5318c
SERGIO <checomperez@gmail.com>
2022-09-04 09:38:19 -0500

    Home screen's desing is changed, accounts, expenses and periods screens are created

--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -1,14 +1,74 @@
+import 'package:expensemanager/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final PageController pageController = PageController();
    
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('Esta es la home screen alv'),
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: [
+          AccountsScreen(),
+          ExpensesScreen(),
+          PeriodsScreen()
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 80),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: TextButton(
+                    onPressed: () => pageController.jumpToPage(0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.add),
+                        Text('Cuentas')
+                      ],
+                    )
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                    onPressed: () => pageController.jumpToPage(1),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.add),
+                        Text('Gastos')
+                      ],
+                    )
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                    onPressed: () => pageController.jumpToPage(2),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.add),
+                        Text('Periodos')
+                      ],
+                    )
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,1 +1,4 @@
 export 'home-screen.dart';
+export 'accounts-screen.dart';
+export 'expenses-screen.dart';
+export 'periods-screens.dart';
